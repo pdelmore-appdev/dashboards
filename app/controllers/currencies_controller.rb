12c67b49b97d9b42c0de63419ee67962c0d18290
Paulina Delmore <delmore.paulina@gmail.com>
2023-02-24 18:19:03 +0000

    first and second currency

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -14,4 +14,21 @@ class CurrenciesController < ApplicationController
   end
 
 
+def second_currency 
+  @raw_data = open("https://api.exchangerate.host/symbols").read
+  @parsed_data = JSON.parse(@raw_data)
+  
+  @symbols_hash = @parsed_data.fetch("symbols")
+  
+  @array_of_symbols = @symbols_hash.keys
+
+
+@first_symbol = params.fetch("currency_symbol")
+
+
+  render({ :template => "currency_templates/step_two.html.erb" })
+end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ get("/", { :controller => "application", :action => "index" })
 get("/forex", { :controller => "currencies", :action => "first_currency" })
 
 
+
+get("/forex/:currency_symbol", { :controller => "currencies", :action => "second_currency" })
+
+het("/forex/:firdt_currency/:second_currency", {})
+
 end
